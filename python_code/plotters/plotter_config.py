abad7bc517c30b968acdd05b78df0976310f6b3f
tomer <tomerraviv95@gmail.com>
2023-02-16 17:54:37 +0200

    Modified the configs of long and short codes to reflect the changes

--- python_code/plotters/plotter_config.py
+++ python_code/plotters/plotter_config.py
@@ -13,37 +13,43 @@ class PlotType(Enum):
 def get_config(plot_type: PlotType) -> Tuple[List[Dict], list, str, str]:
     if plot_type == PlotType.BY_SNR_LONG_CODE:
         params_dicts = [
-            {'snr': 2, 'detector_type': DecoderType.bp.name},
             {'snr': 4, 'detector_type': DecoderType.bp.name},
+            {'snr': 5, 'detector_type': DecoderType.bp.name},
             {'snr': 6, 'detector_type': DecoderType.bp.name},
+            {'snr': 7, 'detector_type': DecoderType.bp.name},
             {'snr': 8, 'detector_type': DecoderType.bp.name},
-            {'snr': 2, 'detector_type': DecoderType.wbp.name},
-            {'snr': 4, 'detector_type': DecoderType.wbp.name},
-            {'snr': 6, 'detector_type': DecoderType.wbp.name},
-            {'snr': 8, 'detector_type': DecoderType.wbp.name},
-            {'snr': 2, 'detector_type': DecoderType.model_based_bayesian_wbp.name},
+            {'snr': 4, 'detector_type': DecoderType.wbp.name, 'train_blocks_num': 30},
+            {'snr': 5, 'detector_type': DecoderType.wbp.name, 'train_blocks_num': 30},
+            {'snr': 6, 'detector_type': DecoderType.wbp.name, 'train_blocks_num': 30},
+            {'snr': 7, 'detector_type': DecoderType.wbp.name, 'train_blocks_num': 30},
+            {'snr': 8, 'detector_type': DecoderType.wbp.name, 'train_blocks_num': 30},
             {'snr': 4, 'detector_type': DecoderType.model_based_bayesian_wbp.name},
+            {'snr': 5, 'detector_type': DecoderType.model_based_bayesian_wbp.name},
             {'snr': 6, 'detector_type': DecoderType.model_based_bayesian_wbp.name},
+            {'snr': 7, 'detector_type': DecoderType.model_based_bayesian_wbp.name},
             {'snr': 8, 'detector_type': DecoderType.model_based_bayesian_wbp.name},
         ]
-        values = list(range(2, 9, 2))
+        values = list(range(4, 9, 1))
         xlabel, ylabel = 'SNR', 'SER'
     elif plot_type == PlotType.BY_SNR_SHORT_CODE:
         params_dicts = [
-            {'snr': 2, 'detector_type': DecoderType.bp.name},
             {'snr': 4, 'detector_type': DecoderType.bp.name},
+            {'snr': 5, 'detector_type': DecoderType.bp.name},
             {'snr': 6, 'detector_type': DecoderType.bp.name},
+            {'snr': 7, 'detector_type': DecoderType.bp.name},
             {'snr': 8, 'detector_type': DecoderType.bp.name},
-            {'snr': 2, 'detector_type': DecoderType.wbp.name},
-            {'snr': 4, 'detector_type': DecoderType.wbp.name},
-            {'snr': 6, 'detector_type': DecoderType.wbp.name},
-            {'snr': 8, 'detector_type': DecoderType.wbp.name},
-            {'snr': 2, 'detector_type': DecoderType.model_based_bayesian_wbp.name},
+            {'snr': 4, 'detector_type': DecoderType.wbp.name, 'train_blocks_num': 30},
+            {'snr': 5, 'detector_type': DecoderType.wbp.name, 'train_blocks_num': 30},
+            {'snr': 6, 'detector_type': DecoderType.wbp.name, 'train_blocks_num': 30},
+            {'snr': 7, 'detector_type': DecoderType.wbp.name, 'train_blocks_num': 30},
+            {'snr': 8, 'detector_type': DecoderType.wbp.name, 'train_blocks_num': 30},
             {'snr': 4, 'detector_type': DecoderType.model_based_bayesian_wbp.name},
+            {'snr': 5, 'detector_type': DecoderType.model_based_bayesian_wbp.name},
             {'snr': 6, 'detector_type': DecoderType.model_based_bayesian_wbp.name},
+            {'snr': 7, 'detector_type': DecoderType.model_based_bayesian_wbp.name},
             {'snr': 8, 'detector_type': DecoderType.model_based_bayesian_wbp.name},
         ]
-        values = list(range(2, 9, 2))
+        values = list(range(4, 9, 1))
         xlabel, ylabel = 'SNR', 'SER'
     else:
         raise ValueError('No such plot type!!!')
