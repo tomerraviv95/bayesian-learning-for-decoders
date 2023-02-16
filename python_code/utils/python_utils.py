ea8083d1f8fd6b66806425eb079c0262df0d3b46
tomer <tomerraviv95@gmail.com>
2023-02-16 17:41:08 +0200

    Removed the filtering operation

--- python_code/utils/python_utils.py
+++ python_code/utils/python_utils.py
@@ -3,9 +3,6 @@ import pickle as pkl
 from typing import Dict, Any
 
 import numpy as np
-import torch
-
-from python_code import DEVICE
 
 
 def load_code_parameters(bits_num, parity_bits_num, ecc_mat_path, tanner_graph_cycle_reduction):
@@ -18,18 +15,6 @@ def load_code_parameters(bits_num, parity_bits_num, ecc_mat_path, tanner_graph_c
     if tanner_graph_cycle_reduction:
         code_pcm = (np.load(ecc_path + '_PCM_CR.npy')).astype(np.float32)
     return code_pcm, code_gm
-
-
-def llr2bits(llr_vector):
-    return torch.round(torch.sigmoid(-llr_vector))
-
-
-def syndrome_condition(unsatisfied, llr_words, code_pcm):
-    words = llr2bits(llr_words).float()
-    syndrome = torch.fmod(torch.mm(words, torch.tensor(code_pcm.T).float().to(device=DEVICE)), 2)
-    equal_flag = ~torch.eq(torch.sum(torch.abs(syndrome), dim=1), torch.FloatTensor(1).fill_(0).to(device=DEVICE))
-    new_unsatisfied = unsatisfied[equal_flag]
-    return new_unsatisfied
 
 
 def save_pkl(pkls_path: str, array: np.ndarray, type: str):
